7fc54ba617020e18cf3a0dcad492eb313ff3a388
Ana Luiza Santos <anaguimaraes041@gmail.com>
2026-09-23 09:24:52 -0300

    sistema de xp

--- main.py
+++ main.py
@@ -52,4 +52,96 @@ async def on_ready():
 
         await canal.send(embed=embed)
 
-fiot.run(funciona)
+
+#============================TABELA=XP=================================#
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS usuarios (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL, 
+                usuario TEXT NOT NULL,
+                xp INT NOT NULL,
+                nivel INT NOT NULL)""")
+conn.commit()
+
+
+#============================COOLDOWNS=================================#
+
+cooldowns = {}
+COOLDOWNS_SEGUNDOS = 30
+xpImagem = 15
+
+#verificação imagem
+def temImagem(message):
+    if not message.attachments:
+        return False
+    
+    extensoesImagem = (".png",".jpg", ".jpeg", ".gif", ".webp")
+
+    for anexo in message.attachments:
+        if anexo.filename.lower().endswith(extensoesImagem):
+            return True
+    return False
+    
+#xp por caractere por
+@fiot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    
+    nome = str(message.author.display_name)
+    userID = str(message.auhtor.id)
+
+    agora = time.time()
+
+    if userID in cooldowns and agora - cooldowns[userID] < COOLDOWNS_SEGUNDOS:
+        pass
+    else:
+        cooldowns[userID] = agora 
+
+    xpGanho = 0
+
+    if temImagem(message):
+        xpGanho += xpImagem
+
+    tamanhoMensagem = len(message.content)
+    xpGanho += tamanhoMensagem
+
+    if xpGanho == 0:
+        await fiot.process_commands(message)
+
+    cursor.execute("""SELECT xp, nivel FROM usuarios
+                    WHERE usuario = ?, (userID, )""")
+    
+    resultado = cursor.fetchone()
+
+    if resultado is None:
+        cursor.execute("""INSERT INTO usuarios (nome, usuario, xp, nivel)
+                        VALUES (?, ?, ?, ?)""", (nome, userID, xpGanho, 1))
+        
+    else:
+        xpAtual, nivelAtual = resultado
+        novoXP = xpAtual + xpGanho
+        novoNivel = nivelAtual
+        xpNecessario = nivelAtual
+
+        xpNecessario = novoNivel * 100
+
+    while novoXP >= xpNecessario:
+        novoXP -= xpNecessario
+        novoNivel += 1
+        xpNecessario = novoNivel * 100
+
+    cursor.execute("""UPDATE usuarios SET xp = ?, nivel = ?, nome = ?
+                    WHERE usuario = ?""", (novoXP, novoNivel, nome, userID))
+    
+    if novoNivel > nivelAtual:
+        
+        embed = discord.Embed(title = "SUBIU DE NÍVEL", description = f"infelizmente {message.author.mention}, subiu para o nível **{novoNivel}**")
+        
+        embed.color = discord.Color.gold()
+
+        embed.set_footer(text="continue conversando para subir de nível")
+
+        await message.channel.send(embed)
+    
+    conn.commit()
